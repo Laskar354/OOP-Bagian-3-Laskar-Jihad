ae2c81925f9b8ab44e17fca36f7f1c84e1533e28
Laskar354 <61928924+Laskar354@users.noreply.github.com>
2020-05-08 14:40:39 +0700

    Create PronterWindows.cs

---
+++ PronterWindows.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorp
+{
+    class PrinterWindows
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("\nPrinter display dimension");
+        }
+        public virtual void print()
+        {
+            Console.WriteLine("Printer printing");
+        }
+    }
+}
+© 2020 GitHub, Inc.
